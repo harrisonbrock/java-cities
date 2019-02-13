8b6bf6d4d1d8498b015a936c42085fdff3af7b18
Harrison Brock <harrison@harrisonbrock.com>
2019-02-13 16:20:00 -0500

    fixed typo

--- src/main/java/com/harrisonbrock/citiesapp/controllers/CityController.java
+++ src/main/java/com/harrisonbrock/citiesapp/controllers/CityController.java
@@ -3,5 +3,5 @@ package com.harrisonbrock.citiesapp.controllers;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class City {
+public class CityController {
 }
